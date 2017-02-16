877505aafe1528c234e66a65a86b7a4f2a06eb09
Anurag Kumar <akanuragkumar712@gmail.com>
2017-02-17 01:34:38 +0530

    module add

---
+++ Website/js/demo.js
@@ -0,0 +1,11 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#1a8af3',
+    lineColor: '#1a8af3'
+  });
+}, false);
